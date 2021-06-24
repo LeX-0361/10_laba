50bfe09d3f7a9ceac7bf9d40eb4c2800c3370de1
LeX-0361 <81358029+LeX-0361@users.noreply.github.com>
2021-06-25 01:08:04 +0300

    Update skills.js

--- skills.js
+++ skills.js
@@ -8,15 +8,15 @@ fetch('db/skills.json')
         skills.forEach(item => {
 
             const dt = document.createElement('dt');
-            dt.textContent = skill.name;
-            dt.classList.add(skill.css);
+            dt.textContent = item.name;
+            dt.classList.add(item.css_class);
 
             const dd = document.createElement('dd');
             dd.classList.add('level');
 
             const div = document.createElement('div');
-            div.style.width = skill.level + '%';
-            div.textContent = skill.level;
+            div.style.width = item.level + '%';
+            div.textContent = item.level;
 
             dd.appendChild(div);
             dl.append(dt);
